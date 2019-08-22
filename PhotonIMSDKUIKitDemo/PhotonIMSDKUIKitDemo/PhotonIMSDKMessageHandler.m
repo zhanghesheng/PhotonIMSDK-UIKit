343cffbbf87fbf0dff0c85cd1873a2ee26a32eb6
zhangshuisheng <zhang.shuisheng@immomo.com>
2019-08-22 13:46:34 +0800

    提交代码

--- PhotonIMSDKUIKitDemo/PhotonIMSDKUIKitDemo/PhotonIMSDKMessageHandler.m
+++ PhotonIMSDKUIKitDemo/PhotonIMSDKUIKitDemo/PhotonIMSDKMessageHandler.m
@@ -27,4 +27,11 @@
         completion(YES,@"D6813718-1155-6160-DE1D-142ABCD62A30");
     }
 }
+
+- (void)uploadImage:(NSString *)filePath completion:(void (^)(BOOL, NSString * _Nullable, NSString * _Nullable))completion{
+    if (completion) {
+    completion(YES,@"http://img.momocdn.com/moment/07/72/07722C60-C9F7-31A4-DB5C-7B6A3124FA8020190822_L.jpg",nil);
+    
+    }
+}
 @end
